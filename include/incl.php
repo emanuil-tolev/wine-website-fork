98b831b54fe5e9918e2804f5f6eeeba355a002cb
Jeremy Newman <jnewman@winehq.org>
2003-04-22 13:48:37 +0000

    removed the session code and decided to use a cookie for the theme code for now no reason to use mysql just to store session data, I may go back to the session code when there is more user prefs than just the theme.

--- include/incl.php
+++ include/incl.php
@@ -19,47 +19,21 @@ $config = new config();
 
 // load global libs
 require($file_root."/include/"."html.php");
-require($file_root."/include/"."db.php");
-require($file_root."/include/"."session.php");
 require($file_root."/include/"."sidebar.php");
-require($file_root."/include/"."winehq.php");
 require($file_root."/include/"."wwn.php");
 require($file_root."/include/"."utils.php");
 
 // create html object
 $html = new html($file_root);
 
-// connect to store database
-$db = new db($config->db_user, $config->db_pass, $config->db_host, $config->db_name);
-
-// create winehq object (for misc var storage in session)
-$winehq = new winehq();
-
-// Init Session (stores crap into winehq session object)
-$session = new session("winehq");
-$session->register("winehq");
-
 // load the theme from the session
-if (isset($winehq->theme)) $config->theme = $winehq->theme;
-
-// load the language from the session
-if (isset($winehq->lang)) $config->lang = $winehq->lang;
-
-// dump the session message buffer to user
-$html->status_message = $session->dumpmsgbuffer();
+if (isset($_COOKIE['winehq'])) $config->theme = $_COOKIE['winehq'];
 
 // set the theme from get url
-if (isset($theme) and in_array($theme, $config->themes))
+if (isset($_GET['theme']) and in_array($_GET['theme'], $config->themes))
 {
-    $config->theme = $theme;
-    $winehq->theme = $theme;
-}
-
-// set the language from get url
-if (isset($lang) and in_array($lang, $config->languages))
-{
-    $config->lang = $lang;
-    $winehq->lang = $lang;
+    setcookie("winehq", $_GET['theme'], time()+((3600*12)*365), "/");
+    $config->theme = $_GET['theme'];
 }
 
 // load a banner ad
