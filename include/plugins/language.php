a6cc111129a8dd7123bd0bbebc06fdb0aecd0caf
Jeremy Newman <jnewman@codeweavers.com>
2008-12-11 10:22:47 -0600

    limit cookie to the base_root of the site

--- include/plugins/language.php
+++ include/plugins/language.php
@@ -11,7 +11,7 @@ global $html, $config;
 // if specified, switch to lang
 if (defined('PAGE_PARAMS') and in_array(PAGE_PARAMS, $config->languages))
 {
-    setcookie("lang", PAGE_PARAMS, time()+60*60*24*365, '/');
+    setcookie("lang", PAGE_PARAMS, time()+60*60*24*365, "{$config->base_root}/");
     $html->redirect($config->base_url);
     exit();
 }
