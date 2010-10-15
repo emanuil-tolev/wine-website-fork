35b9f6c2b556df42cb935f0d63a6126dbe87bcaa
Jeremy Newman <jnewman@codeweavers.com>
2010-10-15 09:35:12 -0500

    language plugin now has a list command

--- include/plugins/language.php
+++ include/plugins/language.php
@@ -6,14 +6,32 @@
     by Jeremy Newman <jnewman@codeweavers.com>
 */
 
-global $html, $config;
+global $html, $config, $data;
 
-// if specified, switch to lang
-if (defined('PAGE_PARAMS') and in_array(PAGE_PARAMS, $config->languages))
+switch ($this->params['cmd'])
 {
-    setcookie("lang", PAGE_PARAMS, time()+60*60*24*365, "{$config->base_root}/");
-    $html->redirect($config->base_url);
-    exit();
+    // list languages
+    case "list":
+        foreach ($config->languages as $lang)
+        {
+            $row = array(
+                         'lang'     => $lang,
+                         'langFull' => $data->languages[$lang]['name'],
+                         'langHelp' => $data->languages[$lang]['change']
+                        );
+            echo $html->template('local', 'global/lang_row', $row, 1);
+        }
+        break;
+
+    // set the language based on the URL
+    default:
+        // if specified, switch to lang
+        if (defined('PAGE_PARAMS') and in_array(PAGE_PARAMS, $config->languages))
+        {
+            setcookie("lang", PAGE_PARAMS, time()+60*60*24*365, "{$config->base_root}/");
+            $html->redirect($config->base_url);
+            exit();
+        }
 }
 
 ?>
