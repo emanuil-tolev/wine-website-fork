5d3f0d0ecdf591b011423b099f4a5627140c3111
Jeremy Newman <jnewman@codeweavers.com>
2012-04-12 14:24:45 -0500

    use class autoloading, add some error handling functions

--- include/incl.php
+++ include/incl.php
@@ -10,23 +10,32 @@
  * Main Include Library
  */
 
-// load config class
-require("{$file_root}/include/config.php");
-
-// create config object
-$config = new config($file_root."/include/"."winehq.conf", $file_root."/include/"."globals.conf");
+// Require PHP version 5.3 or higher
+if (version_compare(phpversion(), '5.3.0') < 0)
+{
+    trigger_error("PHP 5.3 or Higher Required!", E_USER_ERROR);
+}
 
 // load global functions lib
 require_once("{$file_root}/include/utils.php");
 
-// load data lib
-check_and_require("data");
-$data = new data();
+// Set Up the Class AutoLoader
+spl_autoload_register("check_and_require");
 
-// load html lib
-check_and_require("html");
+// create config object
+$config = new config("{$file_root}/include/winehq.conf", "{$file_root}/include/globals.conf");
+
+// load data lib
+$data = new data();
 
 // create html object
 $html = new html($file_root);
+
+// setup html error handler when not at CLI
+if (!defined('STDIN'))
+{
+    set_error_handler(array(&$html, 'error_handler'));
+    register_shutdown_function(array(&$html, 'error_shutdown'));
+}
 
 ?>
