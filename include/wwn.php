f7ea018d4de81b50b4fa68e16f3586f6d92b4636
Jeremy Newman <jnewman@codeweavers.com>
2012-04-12 14:37:03 -0500

    update object to PHP 5 style, fix depreciated code

--- include/wwn.php
+++ include/wwn.php
@@ -8,51 +8,51 @@
 
 class wwn 
 {
-    var $currentTag;
-    var $issue;
-    var $who;
-    var $author;
-    var $email;
-    var $contact;
-    var $body;
-    var $summary;
-    var $person;
-    var $map_array;
-    var $inquote = 0;
-    var $mode = 'wwn';
-    
-    // init wwn object
-    function wwn ()
+    public $mode = 'wwn';
+    public $currentTag;
+    public $issue;
+    public $who;
+    public $author;
+    public $email;
+    public $contact;
+    public $body;
+    public $summary;
+    public $person;
+    private $map_array;
+    private $inquote = 0;
+
+    // constuctor
+    public function __construct ()
     {
         $this->summary = array();
         $this->person = array();
         $this->map_array = array(
-                                 "a"	        => "a",
-                                 "b"	        => "b",
-                                 "blockquote"	=> "blockquote",
+                                 "a"            => "a",
+                                 "b"            => "b",
+                                 "blockquote"   => "blockquote",
                                  "center"       => "center",
                                  "code"         => "code",
                                  "div"          => "div",
-                                 "font"          => "font",
-                                 "i"	        => "i",
+                                 "font"         => "font",
+                                 "i"            => "i",
                                  "img"          => "img",
-                                 "li"	        => "li",
+                                 "li"           => "li",
                                  "table"        => "table",
-                                 "tr"	        => "tr",
-                                 "td"	        => "td",
+                                 "tr"           => "tr",
+                                 "td"           => "td",
                                  "th"           => "th",
                                  "dt"           => "dt",
                                  "dd"           => "dd",
                                  "tt"           => "tt",
                                  "pre"          => "pre",
-                                 "u"	        => "u",
-                                 "ul"	        => "ul",
-                                 "ol"	        => "ol",
+                                 "u"            => "u",
+                                 "ul"           => "ul",
+                                 "ol"           => "ol",
                                 );
     }
-    
+
     // read dir and get issues
-    function get_list ()
+    public function get_list ()
     {
         global $config;
         $wwn = array();
@@ -71,11 +71,11 @@ class wwn
             switch ($this->mode)
             {
                 case "interviews":
-                    $issue = ereg_replace("interview_([0-9]+)\.xml", "\\1", $file);
+                    $issue = preg_replace("/interview_([0-9]+)\.xml/", "\\1", $file);
                     break;
-                
+
                 default:
-                    $issue = ereg_replace("wn[0-9]+_([0-9]+)\.xml", "\\1", $file);
+                    $issue = preg_replace("/wn[0-9]+_([0-9]+)\.xml/", "\\1", $file);
             }
             if ($issue > $old)
                 $cur = $issue;
@@ -89,10 +89,10 @@ class wwn
     }
 
     // show back issues
-    function issues_list ($issues, $cur, $limit = 0, $pos = 0)
+    public function issues_list ($issues, $cur, $limit = 0, $pos = 0)
     {
         global $file_root, $config, $html;
-    
+
         // number of issues to show per page
         switch ($this->mode)
         {
@@ -102,23 +102,23 @@ class wwn
             default:
                 $perpage = 12;
         }
-        
+
         // total issues
         $total = count($issues);
-        
+
         // couters
         $where = 0;
         $c = 0;
         $num = -1;
-        
+
         if ($limit == 0)
-          $back = '<div align=center><table cellpadding=0 cellspacing=20 width="100%"><tr valign=top>'."\n";	
-        
+          $back = '<div align=center><table cellpadding=0 cellspacing=20 width="100%"><tr valign=top>'."\n";
+
         while(list($issue,$file) = each($issues))
         {
             // inner counter
             $num++;
-    
+
             if ($limit != 0 && $c == $limit)
             {
               // break out if we hit our limit
@@ -129,36 +129,36 @@ class wwn
               // do not show images less than current pos
               continue;
             }
-    
+
             // display summary
             $this->wwn_xml_parse($file);
             $summary_box = $this->format_summary($issue, $this->issue);
-            
+
             if ($limit == 0)
               $back .= "\n\n<td>\n".$summary_box."\n</td>\n";
             else
               $back .= "\n\n".$summary_box."\n";
-              
+
             // outer counter
             $c++;
-    
+
             // end row at 3
             if (($limit == 0) && ($c % 3 == 0) && ($c != 1))
             {
                 $back .= "</tr><tr valign=top>\n";
             }
-            
+
             // end at max
             if ($c == $perpage)
             {
                 $where = $num;
                 break;
-            }			
+            }
         }
-        
+
         if ($limit == 0)
           $back .= '</tr></table></div><br>'."\n";
-        
+
         if ($limit != 0)
         {
             $back .= "<p class=\"small\"><a href=\"".$html->_web_root."/".PAGE."/"."\">More Issues...</a></p>";
@@ -167,7 +167,7 @@ class wwn
         {
             $nextLink = "&nbsp;";
             $nextLink = "&nbsp;";
-    
+
             // display prev link
             if ($pos >= $perpage)
             {
@@ -178,7 +178,7 @@ class wwn
             {
                 $prevLink = "<span class=\"disabled\">&lt;&lt; Prev $perpage Issues</span>";
             }
-    
+
             // display next link
             if (($pos + $perpage) < $total)
             {
@@ -189,34 +189,34 @@ class wwn
             {
                 $nextLink = "<span class=\"disabled\">Next $perpage Issues &gt;&gt;</span>";
             }
-            
-        
+
+
             $back .= "<div align=\"center\"><table width=\"50%\"><tr>\n".
                      "<td align=\"left\">&nbsp; $prevLink</td>".
                      "<td align=\"right\">&nbsp; $nextLink</td>".
                      "</tr></table></div>\n";
         }
-            
+
         return $back;
     }
-    
+
     // format the summary box
-    function format_summary ($cur, $date = null)
+    private function format_summary ($cur, $date = null)
     {
         global $html;
-        
+
         switch ($this->mode)
         {
             case "interviews":
                 $summary_box = '<a href="'.$html->_web_root."/".PAGE."/".$cur.'"><b>'.$this->who.'</b></a>'.
                                '<br><span class=small>'.$date.'</span><ul type="circle">'."\n";
                 break;
-            
+
             default:
                 $summary_box = '<a href="'.$html->_web_root."/".PAGE."/".$cur.'"><b>Issue: '.$cur.'</b></a>'.
                                '<br><span class=small>'.$date.'</span><ul type="circle">'."\n";
         }
-        
+
         $c = 0;
         while(list($id,$sum) = each($this->summary))
         {
@@ -226,35 +226,35 @@ class wwn
         $summary_box .= "</ul>";
         return $summary_box;
     }
-    
+
     // display a single wwn issue
-    function view_issue ($view = "latest")
+    public function view_issue ($view = "latest")
     {
         global $config, $html, $cur;
-    
+
         // read dir and get issues
         list($wwn, $cur) = $this->get_list();
-    
+
         // view back issues
         if ($view == 'back')
         {
             $html->template_title = 'WWN Back Issues';
             return $this->issues_list($wwn, $cur, 0, $_REQUEST['pos']);
         }
-        
+
         // show selected issue, or current
         if ($view and $view != 'latest' and $wwn[$view])
           $cur = $view;
 
         // get issue
         $this->wwn_xml_parse($wwn[$cur]);
-        
+
         // get summary
         $summary_box = $this->format_summary($cur);
-        
+
         // title for page
         $html->template_title = 'WWN Issue #'.$cur;
-        
+
         // load issue into template
         $wwn_vars = array(
                           'issue'   => $cur,
@@ -268,12 +268,12 @@ class wwn
 
         return $html->template("base", "wwn_content", $wwn_vars);;
     }
-    
+
     // display a single wwn issue
-    function view_interview($interview)
+    public function view_interview($interview)
     {
         global $config, $html;
-    
+
         // read dir and get issues
         list($wwn, $cur) = $this->get_list("interviews");
 
@@ -283,13 +283,13 @@ class wwn
             $html->in404 = 1;
             return $html->template("base", "404");
         }
-        
+
         // get issue
         $this->wwn_xml_parse($wwn[$interview]);
-        
+
         // title for page
         $html->template_title = $this->who.' Interview';
-        
+
         // load issue into template
         $wwn_vars = array(
                           'who'    => $this->who,
@@ -297,15 +297,15 @@ class wwn
                           'author' => $html->ahref($this->author, 'mailto:'.$this->email),
                           'body'   => $this->body,
                          );
-        
+
         return $html->template("base", "wwn_interview", $wwn_vars);
     }
-    
+
     // read the xml file and parse it
-    function wwn_xml_parse ($file)
+    private function wwn_xml_parse ($file)
     {
         global $config, $html;
-        
+
         // load file from language
         switch ($this->mode)
         {
@@ -316,7 +316,7 @@ class wwn
                 else
                     $file = $config->wwn_xml_path."/".$config->lang."/interviews/".$file;
                 break;
-            
+
             // wwn mode
             default:
                 if (file_exists($config->wwn_xml_path."/".$html->lang."/".$file))
@@ -324,34 +324,34 @@ class wwn
                 else
                     $file = $config->wwn_xml_path."/".$config->lang."/".$file;
         }
-        
+
         $this->issue = "";
         $this->body = "";
         $this->who = "";
         $this->summary = array();
-    
+
         // initialize parser
         $xml_parser = xml_parser_create();
-    
+
         // set callback functions
-        xml_set_object($xml_parser, &$this);
+        xml_set_object($xml_parser, $this);
         xml_set_element_handler($xml_parser, "startElement", "endElement");
         xml_set_character_data_handler($xml_parser, "characterData");
-    
+
         // open XML file
         if (!($fp = fopen($file, "r")))
         {
             $this->body = "Cannot locate XML data file: $file";
             return;
         }
-    
+
         // read and parse data
         while ($data = fread($fp, 16384))
         {
             // translate & entities
             $data = _translateLiteral2NumericEntities($data);
             $data = _translateWWN2valid($data);
-            
+
             // error handler
             if (!xml_parse($xml_parser, $data, feof($fp)))
             {
@@ -361,19 +361,19 @@ class wwn
                 return;
             }
         }
-    
+
         // clean up
         xml_parser_free($xml_parser);
     }
-    
+
     // process elements in xml
-    function startElement ($parser, $name, $attrs)
+    private function startElement ($parser, $name, $attrs)
     {
         global $html;
-    
+
         // set current tag
         $this->currentTag = $name;
-        
+
         // output opening HTML tags
         switch ($name)
         {
@@ -390,7 +390,7 @@ class wwn
               break;
             case "INTRO":
               $this->body .= "<a name=\"Intro\"></a>\n";
-              break;	
+              break;
             case "SECTION":
               array_push($this->summary, $attrs{'TITLE'});
               $this->body .= $html->br();
@@ -464,7 +464,7 @@ class wwn
                 {
                     $attribs .= " $key=\"$value\"";
                 }
-                $this->body .= "<".$this->map_array[strtolower($name)].$attribs.">";		  
+                $this->body .= "<".$this->map_array[strtolower($name)].$attribs.">";
               }
               else
               {
@@ -473,11 +473,11 @@ class wwn
               break;
         }
     }
-    
-    function endElement ($parser, $name)
+
+    private function endElement ($parser, $name)
     {
         global $html;
-        
+
         // output closing HTML tags
         switch ($name)
         {
@@ -487,7 +487,7 @@ class wwn
             case "ISSUE":
             case "PERSON":
             case "INTRO":
-              break;	
+              break;
             case "SECTION":
               $this->body .= "\n\n</td></tr>\n";
               $this->body .= "</table>";
@@ -545,14 +545,14 @@ class wwn
               }
               break;
         }
-    
+
         // clear current tag variable
         $this->currentTag = "";
     }
-    
+
     // process data between tags
-    function characterData ($parser, $data)
-    {    
+    private function characterData ($parser, $data)
+    {
         // format the data
         switch ($this->currentTag)
         {
@@ -560,10 +560,10 @@ class wwn
               break;
             case "AUTHOR":
               $this->author = $data;
-              break;		  
+              break;
             default:
               $this->body .= $data;
-              break;	  
+              break;
         }
     }
 
@@ -573,7 +573,7 @@ class wwn
 // translate & entities to html numeric values
 function _translateLiteral2NumericEntities ($xmlSource, $reverse = FALSE)
 {
-    static $literal2NumericEntity;    
+    static $literal2NumericEntity;
     if (empty($literal2NumericEntity)) {
       $transTbl = get_html_translation_table(HTML_ENTITIES);
       foreach ($transTbl as $char => $entity) {
@@ -592,9 +592,9 @@ function _translateLiteral2NumericEntities ($xmlSource, $reverse = FALSE)
 function _translateWWN2valid ($xmlSource)
 {
     // fix any stray ampersands
-    $xmlSource = eregi_replace("&", "&#038;", $xmlSource);
+    $xmlSource = preg_replace("/\&/", "&#038;", $xmlSource);
     // strip out comments
-    $xmlSource = eregi_replace("<!-- .* -- />", "", $xmlSource);	
+    $xmlSource = preg_replace("/<\!-- .* -- \/>/", "", $xmlSource);
     return $xmlSource;
 }
 
